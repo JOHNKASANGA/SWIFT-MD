f42f354834bef2371dcf1ad9e781db20ccff222d
unknown <israelizuchukwu17382302@gmail.com>
2026-05-01 04:02:42 +0100

    Add course detail page with materials and test yourself button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage"
 import LevelPage from "./pages/LevelPage"
+import CoursePage from "./pages/CoursePage"
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/level/:level" element={<LevelPage />} />
+        <Route path="/course/:id" element={<CoursePage />} />
       </Routes>
     </BrowserRouter>
   );
